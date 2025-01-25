1a884ead1888e41ad53d7772ed587a60461c9141
Surbhiverma12 <vsurbhi247@gmail.com>
2025-01-25 12:27:50 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://vsurbhi243:qXHtEDbCGMcxT6wU@cluster0.f27sl.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0';
+
+const mongoURI = process.env.MONGODB_URI;
+console.log(mongoURI)
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(url, {
+        await mongoose.connect(mongoURI, {
           serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
           socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
           family: 4 // Use IPv4, sometimes helps with connection issues
