2a6ef9f4211053b9e7d3ed1343a56968afc7b471
Surbhiverma12 <vsurbhi247@gmail.com>
2025-01-30 17:25:47 +0530

    Add Post model

--- backend/routes/PostRoutes.js
+++ backend/routes/PostRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
-const Post = require("../models/Post");
+const Post = require("../models/Post.js");
 const router = express.Router();
 
 // Create a new post
